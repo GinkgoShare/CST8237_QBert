34f57f13b935d9acd77ea86b2ffd1a711d12579c
GinkgoShare <cjw.elliott@gmail.com>
2016-03-26 15:43:51 -0400

    add menu scene

---
+++ Q'Bert/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	public void ChangeSceneWithName(string sceneName) {
+		// Load the specified Scene
+		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneName);
+	}
+}
